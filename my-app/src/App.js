3cdff36d93e29e62ecb7a8ef620e9b6ebb189057
Aleksandr <sanycoxxxx@gmail.com>
2022-02-20 21:48:24 +0200

    add star and cookies

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,43 @@ import SearchPanel from "./components/search-panel/search-panel";
 import AppFilter from "./components/app-filters/app-filter";
 import EmployeesList from "./components/employees-list/employees-list";
 import EmployeesAddForm from "./components/employees-add-form/employees-add-form";
+import { getAllByText } from "@testing-library/react";
+
+class WhoAmI extends Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+        years: 25 
+      }
+  }
+
+  NextYears = () => {
+  console.log('+++');
+  this.setState(state => ({
+    years: state.years + 1
+  }))
+  }
+
+  render() {
+      const {name, surname, link} = this.props;
+      return (
+          <div>
+            <div>
+              <button onClick={this.NextYears}>+++</button> 
+            </div>
+              Hello, my name is {name}, surname - {surname}, my sites - {link}, my age - {this.state.years}
+          </div>
+      )
+  }
+}
+
+
 
 function App() {
   const data = [
-{name: "J" , salary: 800 , increase: false, id: 1},
-{name: "s" , salary: 5000 , increase: true, id: 2},
-{name: "r" , salary: 3000 , increase: false, id: 3}
+{name: "James" , salary: 800 , increase: false, id: 1},
+{name: "Sergey" , salary: 5000 , increase: true, id: 2},
+{name: "Roman" , salary: 3000 , increase: false, id: 3}
   ];
   return (
     <div className="App">
@@ -26,8 +57,12 @@ function App() {
         </div>
         <div className="">
           <EmployeesAddForm/>
+         <WhoAmI name='Alex' surname="Adamovych" link='Facebook'/>
+         <WhoAmI name='Ax' surname="Adh" link='Faceb'/>
         </div>
       </header>
+      
+
     </div>
   );
 }
--- my-app/src/components/employees-list-item/employees-list-item.js
+++ my-app/src/components/employees-list-item/employees-list-item.js
@@ -1,16 +1,50 @@
+import {Component} from 'react'
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary, increase}) => {
-    let classNames = 'list-group-item d-flex justify-content-between';
-    if(increase) {
-        classNames += ' increase'
+class EmployeesListItem extends Component{
+    constructor (props){
+        super(props);
+        this.state = {
+            increase: false,
+            favourite: false
+        }
     }
+
+    favourite = () => {
+    this.setState(({favourite}) => ({
+        favourite: !favourite
+    }))
+}
+
+    onIncr = () => {
+        this.setState(({increase}) => ({
+             increase: !increase
+        }))
+    }
+
+    render () {
+
+        const {name, salary} = this.props;
+        const {increase, favourite} = this.state;
+        let classNames = 'list-group-item d-flex justify-content-between';
+        
+
+        if(favourite) {
+            classNames += ' like'
+        }
+
+        if(increase) {
+            classNames += ' increase'
+        }
+
+    
+
     return (
         <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span onClick={this.favourite} className="list-group-item-label">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
+                <button onClick={this.onIncr} type="button"
                     className="btn-cookie btn-sm ">
                     <i className="fas fa-cookie"></i>
                 </button>
@@ -23,6 +57,7 @@ const EmployeesListItem = ({name, salary, increase}) => {
             </div>
         </li>
     )
+    }
 }
 
 export default EmployeesListItem;
